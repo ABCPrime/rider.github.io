e758a5d22b14f3b543032c5374c7ae15bdf2d431
ABCPrime <s1091745@gm.pu.edu.tw>
2023-11-15 17:51:28 +0800

    123

--- home.js
+++ home.js
@@ -14,22 +14,3 @@ menu.onclick =() =>{
     menu.classList.toggle('fa-times');
     navbar.classList.toggle('active');
 }
-import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
-
-const auth = getAuth();
-signInWithPopup(auth, provider)
-	.then((result) => {
-		// 登入成功，取得 token、user
-		const credential = GoogleAuthProvider.credentialFromResult(result);
-		const token = credential.accessToken;
-		const user = result.user;
-		console.log(token);
-		console.log(user);
-	}).catch((error) => {
-		const errorCode = error.code;
-		const errorMessage = error.message;
-		const credential = GoogleAuthProvider.credentialFromError(error);
-		console.log(errorCode);
-		console.log(errorMessage);
-		console.log(credential);
-	});
